2e58bef7fdbf11707c54c512699be270c2452fe2
Jacob Feldman <jacobgfeld@gmail.com>
2018-11-07 19:40:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,42 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
+  def initialize(name = "")
+    @name = name;
+    save
+  end
+  
+  def self.create
+    self.new
+  end
+  
+  def self.new_by_name(name)
+    self.new(name)
+  end
+  
+  def self.create_by_name(name)
+    self.new(name)
+  end
+  
+  def self.find_by_name(name)
+    self.all.find{|p| p.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name)
+  end
+  
+  def self.alphabetical
+    self.all.sort_by{|p| p.name}
+  end
+  
+  def self.new_from_filename(name)
+    binding.pry
+   self.create_by_name(name.split(".mp3")[0].split(" - ")[1])
+  end
 
   def self.all
     @@all
